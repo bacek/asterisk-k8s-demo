b178aef00026c6885e5fc5f5dafb32ac27e0e85b
Vasily Chekalkin <bacek@bacek.com>
2018-09-24 14:46:59 +1000

    Use bridge

--- live-demo/rtpproxy/entrypoint.sh
+++ live-demo/rtpproxy/entrypoint.sh
@@ -7,7 +7,7 @@ fi
 : ${PRIVATE_IPV4="$(netdiscover -field privatev4 ${PROVIDER})"}
 : ${PUBLIC_IPV4="$(netdiscover -field publicv4 ${PROVIDER})"}
 
-: ${RTPPROXY_ARGS:="-f -A ${PUBLIC_IPV4} -F -l ${PRIVATE_IPV4} -m 20000 -M 30000 -s udp:127.0.0.1:7722 -d INFO"}
+: ${RTPPROXY_ARGS:="-f  -F -l  ${PUBLIC_IPV4}/${PRIVATE_IPV4} -m 20000 -M 30000 -s udp:127.0.0.1:7722 -d INFO"}
 
 # If we were given arguments, run them instead
 if [ $# -gt 0 ]; then
